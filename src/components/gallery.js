30a35d734cdcadd89800c4ebe6905cbd0b223743
JoonHyuk Lee <60730765+anshqhsh@users.noreply.github.com>
2022-04-01 21:36:42 +0900

    기본 레이아웃 구현
    사진 데이터 출력, 모달창 띄우기, 레이아웃 수정

---
+++ src/components/gallery.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import './gallery.css';
+import { useEffect, useState } from 'react';
+
+const Gallery = () => {
+  const [images, setImages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const [model, setModel] = useState(false);
+  const [tempimgSrc, setTempimgSrc] = useState();
+
+  // image 데이터를 받아옴
+  const fetchImages = async () => {
+    try {
+      setError(null);
+      setImages(null);
+      setLoading(true);
+      const response = await axios.get(
+        'https://s3.us-west-2.amazonaws.com/secure.notion-static.com/a7804447-abeb-473e-be8b-8025c4f624f6/test.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20220401%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20220401T064523Z&X-Amz-Expires=86400&X-Amz-Signature=a169cfec9b78caa79b729a462e52c25adafa36d19c19979b1402a18e3e15857e&X-Amz-SignedHeaders=host&response-content-disposition=filename%20%3D%22test.json%22&x-id=GetObject'
+      );
+      setImages(response.data);
+    } catch (e) {
+      setError(e);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
+  if (loading) return <div>로딩중..</div>;
+  if (error) return <div>에러가 발생했습니다</div>;
+  // 아직 image가 받아와 지지 않았을 때는 아무것도 표시되지 않도록 해줍니다.
+  if (!images) return null;
+
+  const getImg = imgSrc => {
+    setTempimgSrc(imgSrc);
+    setModel(true);
+    console.log(imgSrc);
+  };
+
+  return (
+    <>
+      <div className={model ? 'model open' : 'model'}>
+        <img src={tempimgSrc}></img>
+      </div>
+      <div className="gallery">
+        {images.renderings.map(image => (
+          <div
+            className="imgContainer"
+            key={image._id}
+            onClick={() => getImg(image._id)}
+          >
+            <img className="image" src={image._id} style={{ width: '100%' }} />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default Gallery;
